9e6f71d73e0b0ca6a3dcc60cbc000073000e43eb
Manel Caireta <m.caireta@altostratus.es>
2025-11-19 12:23:29 +0100

    docs: add command documentation

--- src/gcp_auth/main.py
+++ src/gcp_auth/main.py
@@ -2,6 +2,7 @@ import sys
 from collections.abc import Iterator
 from contextlib import contextmanager
 from pathlib import Path
+from typing import Annotated
 
 import typer
 from rich import print  # noqa: A004
@@ -24,26 +25,49 @@ def handle_errors() -> Iterator:
         raise typer.Exit(code=1) from e
 
 
+ProfileArgument = Annotated[str, typer.Argument(help="Name of the profile.")]
+ForceOption = Annotated[bool, typer.Option(help="Overwrite existing profile.")]
+
+
 @app.command()
-def create(profile_name: str, *, force: bool = False) -> None:
+def create(profile: ProfileArgument, *, force: ForceOption = False) -> None:
+    """
+    Creates a new profile in the vault.
+
+    Arguments:
+        force(bool): Whether to overwrite an existing profile with the same name.
+    """
+
     with handle_errors():
-        vault.register(Profile(name=profile_name), force=force)
+        vault.register(Profile(name=profile), force=force)
 
 
 @app.command()
 def list() -> None:  # noqa: A001
+    """
+    Lists all profiles in the vault.
+    """
+
     with handle_errors():
         for profile in vault.list_profiles():
             print(profile.name)
 
 
 @app.command()
-def activate(profile_name: str) -> None:
+def activate(profile: ProfileArgument) -> None:
+    """
+    Activates a profile.
+    """
+
     with handle_errors():
-        vault.set_active_profile(Profile(name=profile_name))
+        vault.set_active_profile(Profile(name=profile))
 
 
 @app.command()
-def delete(profile_name: str) -> None:
+def delete(profile: ProfileArgument) -> None:
+    """
+    Deletes a profile from the vault.
+    """
+
     with handle_errors():
-        vault.delete_profile(Profile(name=profile_name))
+        vault.delete_profile(Profile(name=profile))
